80a076fd2cb50815b624b11b97ced581de374fc9
DEV_YKJ <rudwns7552@gmail.com>
2021-04-03 00:22:36 +0900

    1) querydsl 테스트코드 작성 2) JPAQueryFactory 멤버변수 필드 처리    => EntityManager가 멀티스레드 동시 요청이 오더라도 현재 트랜잭션에 맞는 em를 반환하도록 설계되어 있기 때문에 이슈 없음 3) q객체 생성위해 gradle>other>compileQuerydsl 실행

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,74 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @Autowired
+    EntityManager em;
+
+    //멤버변수로 빼더라도 동시성 이슈 없음
+    //EntityManager는 멀티스레드에 영향을 받지 않게 설계되어 있음
+    //여러 곳에서 동시에 호출 되더라도 em는 현재 트랜잭션에 맞게 바인딩 되도록 처리 되어 있기 때문
+    JPAQueryFactory jpaQueryFactory;
+
+    @BeforeEach
+    public void before() {
+        jpaQueryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라
+        Member findMember = em.createQuery(
+                "select m from Member m " +
+                        "where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        //entity 생성 후 q객체 생성을 위해 gradle>other>compileQuerydsl 실행
+        QMember m = QMember.member;
+
+        Member findMember = jpaQueryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
